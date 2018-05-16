ff350668ee1e286feda88817e7adf75f54ae7fd9
cristi.alexandrescu <necrocris2000@yahoo.com>
2018-05-16 12:44:36 +0300

    select updated in views

--- src/StudentsManagement.Persistence.EF/Migrations/20180516090602_Initial.cs
+++ src/StudentsManagement.Persistence.EF/Migrations/20180516090602_Initial.cs
@@ -3,7 +3,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace IdentityServer.Persistence.EF.Migrations
 {
-    public partial class InitialMigration : Migration
+    public partial class Initial : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
