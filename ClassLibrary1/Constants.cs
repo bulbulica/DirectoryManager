ef0ae125cec0a7cd3a6cd8bae4077e7ba9cb6276
cristi.alexandrescu <necrocris2000@yahoo.com>
2018-05-21 12:25:20 +0300

    constants added

---
+++ ClassLibrary1/Constants.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentityServer.Domain
+{
+    public class Constants
+    {
+        public static string GeneralManagerRole = "GeneralManager";
+
+        public static string DepartmentManagerRole = "DepartmentManager";
+
+        public static string TeamLeaderRole = "TeamLeader";
+
+        public static string DeveloperRole = "Developer";
+
+        public static string QARole = "QA";
+
+        public static string OfficeManagerRole = "OfficeManager";
+    }
+}
